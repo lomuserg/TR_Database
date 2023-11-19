0f08a08502c5be6f5c60d3b9ca3c7e53bb547835
lomuserg <sergeymu2015@yandex.ru>
2023-11-19 16:11:26 +0300

    users_page.ftlh

---
+++ src/main/java/ru/podol/tr_database/controllers/UsersController.java
@@ -0,0 +1,13 @@
+package ru.podol.tr_database.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UsersController {
+    @GetMapping("/users")
+    public String getUsersPage(){
+        return "users_page";
+    }
+
+}
