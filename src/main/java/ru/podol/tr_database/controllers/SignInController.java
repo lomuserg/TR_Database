6f738289c6568f66243fc5309f820cda3daeb625
lomuserg <sergeymu2015@yandex.ru>
2023-11-19 20:17:37 +0300

    Create SignInController with SignIn_page.ftlh

---
+++ src/main/java/ru/podol/tr_database/controllers/SignInController.java
@@ -0,0 +1,12 @@
+package ru.podol.tr_database.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SignInController {
+    @GetMapping("/signIn")
+    public String getSignPage(){
+        return "signUp_page";
+    }
+}
