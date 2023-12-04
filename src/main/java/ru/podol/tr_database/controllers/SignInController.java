1912635a30bef1f44047bbcdec271a65ddb41fd5
lomuserg <sergeymu2015@yandex.ru>
2023-12-04 14:57:15 +0300

    fix Controllers repozitories for other types of Users

--- src/main/java/ru/podol/tr_database/controllers/SignInController.java
+++ src/main/java/ru/podol/tr_database/controllers/SignInController.java
@@ -2,8 +2,6 @@ package ru.podol.tr_database.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-
 @Controller
 public class SignInController {
     @GetMapping("/signIn")
