ca0ff16c27f61f01a6640dd14b6adc584101a77d
lomuserg <sergeymu2015@yandex.ru>
2023-12-04 12:08:16 +0300

    fix OrdersRepozitory

--- src/main/java/ru/podol/tr_database/repozitories/OrdersRepozitory.java
+++ src/main/java/ru/podol/tr_database/repozitories/OrdersRepozitory.java
@@ -2,7 +2,6 @@ package ru.podol.tr_database.repozitories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.podol.tr_database.models.Order;
-import ru.podol.tr_database.models.Product;
 
 public interface OrdersRepozitory extends JpaRepository<Order,Long> {
 }
